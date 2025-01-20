a161e14e6d45d4411e88f482c9c98ddd4fd2361d
AZZAM UDDIN <uazzam69@gmail.com>
2025-01-20 16:59:54 +0530

    created routes for user login and register

---
+++ backend/src/controllers/user.controller.ts
@@ -0,0 +1,65 @@
+import bcrypt  from 'bcrypt';
+import { Request, Response } from "express";
+import User from "../model/user.model";
+import validator from "validator";
+import jwt from "jsonwebtoken";
+
+const registerUser = async (req: Request, res: Response): Promise<any> => {
+    try {
+        const {name, email, password} = req.body; 
+        console.log("route hit");
+        console.log(req.body);
+        if(!name || !email || !password) return res.status(402).json({success:false, message: "Missing details"});
+
+        if(!validator.isEmail(email)) return res.status(402).json({success:false, message: "Invalid details"});
+        if(password.length < 8 || password.length > 16) return res.status(402).json({success:false, message: "Password is of min 8 and max 16 chars"});
+
+        const ifExisting = await User.findOne({email});
+
+        if(ifExisting){
+            return res.status(402).json({success:false, message: "User already exist"}); 
+        };
+
+        const salt = await bcrypt.genSalt(10);
+
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = await User.create({
+            name, email, password: hashedPassword
+        });
+
+        const user_token = jwt.sign({id: newUser._id}, process.env.USER_SECRET!);
+
+        return res.status(200).json({success: true, user_token});
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+const loginUser = async (req: Request, res: Response): Promise<any> => {
+    try {
+        const {email, password} = req.body;
+
+        const user = await User.findOne({email: email});
+
+        if(!user){
+            return res.status(402).json({success:false, message: "User not exist"}); 
+        };
+
+        const ifPasswordMatch = await bcrypt.compare(password, user.password);
+
+        if(!ifPasswordMatch) return res.status(402).json({success:false, message: "Incorrect Password"}); 
+
+        const user_token = jwt.sign({id: user._id}, process.env.USER_SECRET!);
+
+        return res.status(200).json({success: true, user_token});
+
+    } catch (error) {
+        console.log(error);
+        return res.status(401).json({success: false, message: "Error while login"});
+    }
+}
+
+export {registerUser, loginUser};
